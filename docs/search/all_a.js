4ea187c025a8ba59bd3fd3e4ec3bbf540e33d237
uenot <t_ueno@eis.hokudai.ac.jp>
2018-08-29 00:28:04 +0900

    Update documents

--- docs/search/all_a.js
+++ docs/search/all_a.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['labels',['labels',['../classspecify__rect_1_1_polygon_builder.html#a4994ebc00aeb79ac5eea5b6c38785eae',1,'specify_rect::PolygonBuilder']]],
   ['lines',['lines',['../classspecify__rect_1_1_polygon_builder.html#a79a02e4bf8b8ff8c55150b912a0ca2d8',1,'specify_rect::PolygonBuilder']]],
-  ['linesegment',['LineSegment',['../cgutil_8hpp.html#a7634fe1379961a22350dbd7047b2e8c1',1,'LineSegment():&#160;cgutil.hpp'],['../torres__etal__2016_8hpp.html#a7634fe1379961a22350dbd7047b2e8c1',1,'LineSegment():&#160;torres_etal_2016.hpp']]],
-  ['linesegmentvector',['LineSegmentVector',['../cgutil_8hpp.html#a1b95586556e66ecbe3e4c552548d1027',1,'LineSegmentVector():&#160;cgutil.hpp'],['../torres__etal__2016_8hpp.html#a1b95586556e66ecbe3e4c552548d1027',1,'LineSegmentVector():&#160;torres_etal_2016.hpp']]],
+  ['linesegment',['LineSegment',['../cgutil_8hpp.html#a7634fe1379961a22350dbd7047b2e8c1',1,'cgutil.hpp']]],
+  ['linesegmentvector',['LineSegmentVector',['../cgutil_8hpp.html#a1b95586556e66ecbe3e4c552548d1027',1,'cgutil.hpp']]],
   ['localizeintersection',['localizeIntersection',['../cgutil_8hpp.html#a1cdc17f2ebbabc6d236cfd1c45469e6e',1,'cgutil.hpp']]]
 ];
